a663ad49ab487cb5989691668415c531f853a72a
dumi <eigidi100@gmail.com>
2015-04-28 18:48:26 +0200

    modification conflictuelle de test

--- Diagnostic6_Entity/src/fr/afcepf/ai93/diag6/entity/autres/ClasseTestAD.java
+++ Diagnostic6_Entity/src/fr/afcepf/ai93/diag6/entity/autres/ClasseTestAD.java
@@ -3,7 +3,10 @@ package fr.afcepf.ai93.diag6.entity.autres;
 public class ClasseTestAD {
 	
 	private void methodoTest(){
-		System.out.println("Andreea fait des tests-no1");
+		System.out.println("maintenant je crée un conflit");
+		int b=0;
+		b++;
+		System.out.println(b);
 	}
 
 }
